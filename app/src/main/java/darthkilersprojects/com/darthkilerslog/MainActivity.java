e5343b69c74ea9f06776ddf13efa77b3d7806a11
darthkiler <kiril.bejenari@gmail.com>
2019-09-25 10:08:24 +0300

    Get the name of the constant and show the text in the toast

--- app/src/main/java/darthkilersprojects/com/darthkilerslog/MainActivity.java
+++ app/src/main/java/darthkilersprojects/com/darthkilerslog/MainActivity.java
@@ -8,10 +8,20 @@ import darthkilersprojects.com.log.L;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static int qwe=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         L.show("first message","hello");
+
+        L.show(L.Utils.asList("qwe",null,1.5f,true));
+
+        L.show(L.Utils.getConstantName(MainActivity.class,MainActivity.qwe));
+
+        L.showAsToast(this,"toast",-5L);
+
+        L.showAsToast(this,666);
     }
 }
--- log/src/main/java/darthkilersprojects/com/log/L.java
+++ log/src/main/java/darthkilersprojects/com/log/L.java
@@ -1,7 +1,10 @@
 package darthkilersprojects.com.log;
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,27 +18,38 @@ public abstract class L {
     private final static String TAG = "Darthkiler";
 
     public static <T> void show(T object) {
-        show(object!=null?object.getClass().getName().split("\\.")[object.getClass().getName().split("\\.").length - 1]:"null", object);
+        show(Utils.extractClassName(object), object);
     }
 
     public static <T> void show(String message, T object) {
         if (object instanceof List)
             showList(message, (List<?>) object);
         else if (checkNotNULL(object))
-            Log.d(TAG, message + (char) Character.SPACE_SEPARATOR + object);
+            Log.d(TAG, Utils.createMessage(message,object));
+    }
+
+    public static <T> void showAsToast(Context context,String message, T object) {
+        Toast.makeText(context,Utils.createMessage(message,object),Toast.LENGTH_SHORT).show();
+    }
+
+    public static <T> void showAsToast(Context context, T object) {
+        showAsToast(context,Utils.extractClassName(object),object);
     }
 
     private static <T> boolean checkNotNULL(T arg) {
         boolean isNotNull = arg != null;
         if (!isNotNull)
-            Log.d(TAG, "Crash null");
+            Log.d(TAG, "null");
         return isNotNull;
     }
 
     private static void showList(String s, List<?> arg) {
-        if (arg != null)
-            for (Object arg1 : arg)
-                show(s, arg1);
+        if (arg != null) {
+            L.show(s,"{");
+            for (Object object : arg)
+                show(Utils.extractClassName(object), object);
+            Log.d(TAG, "}");
+        }
     }
 
     public static class Utils {
@@ -44,5 +58,27 @@ public abstract class L {
         public static <T> ArrayList<T> asList(T... a) {
             return new ArrayList<T>(Arrays.asList(a));
         }
+
+        public static <T> String getConstantName(Class clazz, T arg) {
+            for (Field field :
+                    clazz.getFields()) {
+                try {
+                    if(field.get(null).equals(arg))
+                        return field.getName();
+                } catch (IllegalAccessException e) {
+                    return "Ooops! Something went wrong!";
+                }
+            };
+            return "Constant does't exist or isn't public";
+        }
+
+        static <T> String createMessage(String message, T object) {
+            return message + (char) Character.SPACE_SEPARATOR + object;
+        }
+
+        static String extractClassName(Object object) {
+            return object != null ? object.getClass().getName().split("\\.")[object.getClass().getName().split("\\.").length - 1] : "null";
+        }
+
     }
 }
